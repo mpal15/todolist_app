f21b46cc102902938eae54d36907842732dbe24f
Mohit pal <mohitpal9513@gmail.com>
2023-11-05 14:33:50 +0530

    edit functionalit

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -6,6 +6,9 @@ const TodoList = () =>{
     let[todo,setTodo] = useState([]);
     const[des,SetDes] = useState('');
     const[date,SetDate] = useState();
+    const[edit,setEdit] = useState(false);
+    const[index,setIndex] = useState(-1);
+
     const navigate = useNavigate();
     useEffect(()=>{
      let user =localStorage.getItem("login")
@@ -75,6 +78,30 @@ const TodoList = () =>{
     setTodo(todo)
 
     }
+    function  handleEditsubmit(e){
+        e.preventDefault();
+        if(index !== -1){
+           todo = todo.map((item,i)=>{
+           if(index===i){
+            item.description = des
+            item.date = date
+           }
+           return item
+            })
+            setTodo(todo);
+            setIndex(-1);
+            setEdit(false);
+            SetDes('')
+            SetDate('');
+        }
+
+    }
+    function handleEdit(index){
+        setIndex(index)
+        setEdit(true)
+
+
+    }
     return(
         <>
        <div className="container">
@@ -106,11 +133,35 @@ const TodoList = () =>{
              <div className="row">
             <div className="col">{item.description}</div>
             <div className="col">{item.date}</div>
-            <div className="col"><button className="btn btn-dark">Edit</button></div>
+            <div className="col"><button className="btn btn-dark" onClick={()=>handleEdit(index)}>Edit</button></div>
             <div className="col"><button className="btn btn-danger" onClick={()=>handledelete(index)}>Delete</button></div>
         
        </div>))}
        </div>
+
+     {edit?<div className="edit">
+     <form onSubmit={(e)=> handleEditsubmit(e)}>
+            <div className="row my-3">
+                <div className="col-3">
+                Description:
+                </div>
+                <div className="col-9">
+                <input type="text"  value = {des} onChange={ (e)=>SetDes(e.target.value)} className="form form-control" placeholder="enter the todo"/>
+                </div>
+            </div>
+            <div className="row my-3">
+                <div className="col-3">
+                Date:
+                </div>
+                <div className="col-9">
+                <input type="date" value={date} onChange={ (e)=>SetDate(e.target.value)} className="form form-control"/>
+                </div>
+            </div>
+           
+           
+            <input type="submit" className="btn btn-primary"/>
+        </form>
+</div>:""}  
         </>
     )
 }
